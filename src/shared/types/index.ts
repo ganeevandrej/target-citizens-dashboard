7ae06eb91f00b5a01c9a5fe2e8c25c948f3a2c2c
ganee <ganeevandrej21@gmail.com>
2026-03-12 20:03:48 +0300

    Добавил модель данных

---
+++ src/shared/types/index.ts
@@ -0,0 +1,18 @@
+export type {
+    Citizen,
+    CitizenAddress,
+    CitizenAddressLabel,
+    CitizenDocument,
+    CitizenEducation,
+    CitizenGender,
+    CitizenListItem,
+    CitizenMaritalStatus,
+    CitizenStatus,
+    FamilyMember,
+    FamilyRelation,
+} from './citizens'
+export type {
+    DashboardData,
+    DashboardDistributionItem,
+    DashboardTimeseriesPoint,
+} from './dashboard'
